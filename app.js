2428b603b34e2b001e811d32260664363c5140e8
wallace-pv <wallace-pv@hotmail.com>
2025-03-25 23:33:17 -0300

    Adicionando eventlistener para o botão de volta ao topo

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const toggleButton = document.getElementById('toggleButton');
     const body = document.body;
+    const btnTopo = document.getElementById('btnTopo'); // Obtém a referência ao botão "voltar ao topo"
 
     toggleButton.addEventListener('click', function() {
         body.classList.toggle('dark-mode');
@@ -11,19 +12,21 @@ document.addEventListener('DOMContentLoaded', function() {
             this.innerText = '🌙 Modo Escuro';
         }
     });
-// Exibir o botão quando o usuário rolar a página
-window.onscroll = function() {
-    let btnTopo = document.getElementById("btnTopo");
-    if (document.documentElement.scrollTop > 300) {
-        btnTopo.style.display = "block";
-    } else {
-        btnTopo.style.display = "none";
+
+    // Exibir o botão quando o usuário rolar a página
+    window.onscroll = function() {
+        if (document.documentElement.scrollTop > 300) {
+            btnTopo.style.display = "block";
+        } else {
+            btnTopo.style.display = "none";
+        }
+    };
+
+    // Função para rolar suavemente para o topo
+    function voltarAoTopo() {
+        window.scrollTo({ top: 0, behavior: "smooth" });
     }
-};
 
-// Função para rolar suavemente para o topo
-function voltarAoTopo() {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-}
-
+    // Adiciona o event listener para o clique no botão "voltar ao topo"
+    btnTopo.addEventListener('click', voltarAoTopo);
 });
